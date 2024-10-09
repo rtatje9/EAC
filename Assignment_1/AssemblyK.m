afadd60983bf7c9ef1ce0deac513d9f8627e8b77
Andreu <andreu.craus@estudiantat.upc.edu>
2024-10-09 11:21:18 +0200

    Changed AssemblyK.m

--- Assignment_1/AssemblyK.m
+++ Assignment_1/AssemblyK.m
@@ -1,4 +1,4 @@
-function K = AssemblyK(COOR,CN) 
+function K = AssemblyK(COOR,CN, rho_val) 
     % COOR: Coordinates matrix, CN: Element connectivity matrix
     nelem = size(CN,1); % Number of elements as the number of rows of the connectivty matrix
     nnode = size(COOR,1); % Number of umber of nodes as it is the number of rows of points in the coordinates
@@ -10,16 +10,16 @@ function K = AssemblyK(COOR,CN)
         COOR_e = COOR(NODES_e); % Corresponding coordinates to the element
         he = COOR_e(2) - COOR_e(1); % Element longitude
         % Elemental matrix
-        Ke = 1/he*[1 -1; -1 1]; % Coefficients matrix (Element stifness matrix). In the practice, the matrix should be accomodated in order to evaluate the right EDO
+        Ke = 1/he*[1 -1; -1 1] - rho_val*he/6*[2 1; 1 2]; 
+        % It has the form Ke = 1/he*[1 -1; -1 1] only in the case of u''+f=0 (and thus has been modified for the case u''+u+f=0).
+        
         % Assembly
-        for a = 1:nnodeE
-            for b = 1:nnodeE % a and b local indexes
-                A = CN(e,a); 
-                B = CN(e,b); % A and B global indexes
-                K(A,B) = K(A,B) + Ke(a,b); % Adding the component of the local stifness mtarix
-            end
-        end
+        % Extract global indices for the current element
+        A = CN(e, :); % Global indices for 'a'
+        B = CN(e, :); % Global indices for 'b'
+        
+        % Add the local stiffness matrix components to the global stiffness matrix
+        K(A, B) = K(A, B) + Ke;
     end
 
-    %Ff = sparse(nnode,1);
 end
